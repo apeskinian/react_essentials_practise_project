fe982968c4396e53a211a8ef24e606e26121c222
Matthew Aitken <matt@itken.net>
2025-06-10 16:29:02 +0100

    Add UserInputs component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react"
+
 import Header from "./components/Header"
+import UserInputs from "./components/UserInputs"
+import ResultTable from "./components/ResultTable"
 
 function App() {
-  return (
+  const [ investmentData, setInvestmentData ] = useState(
+      {
+          initialInvestment: 10000,
+          annualInvestment: 1200,
+          expectedReturn: 6,
+          duration: 10
+      }
+  );
+
+  function handleChange(inputIdentifier, newValue) {
+    setInvestmentData(prevData => {
+        return {
+            ...prevData,
+            [inputIdentifier]: newValue
+        }
+    })
+  }
+
+  return ( <>
     <Header />
+    <UserInputs investmentData={investmentData} onChange={handleChange}/>
+    <ResultTable />
+  </>
   )
 }
 
